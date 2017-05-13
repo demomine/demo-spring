ce7ae0d78a967488b895595a2fbcc894c624a96f
袁其亮 <yuanqiliang@51kik.com>
2017-05-13 16:38:08 +0800

    add test dependency

--- src/test/java/com/lance/demo/spring/TestBase.java
+++ src/test/java/com/lance/demo/spring/TestBase.java
@@ -1,10 +1,20 @@
 package com.lance.demo.spring;
 
 import lombok.extern.slf4j.Slf4j;
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
 
 /**
  * Created by perdonare on 2017/5/11.
@@ -12,10 +22,40 @@ import org.springframework.test.context.junit4.SpringRunner;
 @Slf4j
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {App.class})
-public class TestBase {
+public class TestBase extends DBTestCase {
+    public TestBase(String name) {
+        super(name);
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://192.168.1.72/cloud_credit_bill" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root" );
+        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "xb-12345" );
+        // System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "" );
+    }
 
     @Test
     public void bootstrap() {
+        Mockito.any();
         log.info("framework start success");
     }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(new FileInputStream("/partial.xml"));
+    }
+
+    @Override
+    protected DatabaseOperation getSetUpOperation() throws Exception {
+        return DatabaseOperation.REFRESH;
+    }
+
+    @Override
+    protected DatabaseOperation getTearDownOperation() throws Exception {
+        return DatabaseOperation.NONE;
+    }
+
+  /*  @Override
+    protected void setUpDatabaseConfig(DatabaseConfig config) {
+        config.setProperty(DatabaseConfig.PROPERTY_BATCH_SIZE, new Integer(97));
+        config.setFeature(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, true);
+    }*/
 }
---
+++ src/test/java/com/lance/demo/spring/test/DBUnitTest.java
@@ -0,0 +1,42 @@
+package com.lance.demo.spring.test;
+
+import com.lance.demo.spring.TestBase;
+import org.dbunit.Assertion;
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.filter.DefaultColumnFilter;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+/**
+ * Created by perdonare on 2017/5/12.
+ */
+
+public class DBUnitTest extends TestBase {
+
+    public DBUnitTest(String name) {
+        super(name);
+    }
+
+    @Test
+    public void testDemo() throws Exception{
+        IDataSet databaseDataSet = getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("TABLE_NAME");
+
+        // Load expected data from an XML dataset
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(new File("expectedDataSet.xml"));
+        ITable expectedTable = expectedDataSet.getTable("TABLE_NAME");
+
+        // Assert actual database table match expected table
+        Assertion.assertEquals(expectedTable, actualTable);
+
+        ITable filteredTable = DefaultColumnFilter.includedColumnsTable(actualTable,
+                expectedTable.getTableMetaData().getColumns());
+        Assertion.assertEquals(expectedTable, filteredTable);
+    }
+}
