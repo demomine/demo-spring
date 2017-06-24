061238ac4e1d994566a8653428669ffd1a057cb5
perdoanre <perdonare@163.com>
2017-06-24 19:29:29 +0800

    optimize

--- src/test/java/com/lance/demo/spring/TestBase.java
+++ src/test/java/com/lance/demo/spring/TestBase.java
@@ -22,40 +22,5 @@ import java.io.FileInputStream;
 @Slf4j
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {App.class})
-public class TestBase extends DBTestCase {
-    public TestBase(String name) {
-        super(name);
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://192.168.1.72/cloud_credit_bill" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root" );
-        System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "xb-12345" );
-        // System.setProperty( PropertiesBasedJdbcDatabaseTester.DBUNIT_SCHEMA, "" );
-    }
-
-    @Test
-    public void bootstrap() {
-        Mockito.any();
-        log.info("framework start success");
-    }
-
-    @Override
-    protected IDataSet getDataSet() throws Exception {
-        return new FlatXmlDataSetBuilder().build(new FileInputStream("/partial.xml"));
-    }
-
-    @Override
-    protected DatabaseOperation getSetUpOperation() throws Exception {
-        return DatabaseOperation.REFRESH;
-    }
-
-    @Override
-    protected DatabaseOperation getTearDownOperation() throws Exception {
-        return DatabaseOperation.NONE;
-    }
-
-  /*  @Override
-    protected void setUpDatabaseConfig(DatabaseConfig config) {
-        config.setProperty(DatabaseConfig.PROPERTY_BATCH_SIZE, new Integer(97));
-        config.setFeature(DatabaseConfig.FEATURE_BATCHED_STATEMENTS, true);
-    }*/
+public class TestBase {
 }
